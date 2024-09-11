5219b09c65a0ae3b3f9b9bdda4f2e11165aac5a6
kovacsnador <kovacs.nador@gmail.com>
2024-09-11 13:30:25 +0200

    add promise unit tests

---
+++ test/src/Promsie_test.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+
+#include <concepts>
+
+#include <tinycoro/Promise.hpp>
+
+TEST(PromiseTest, PromiseTest_void)
+{
+    tinycoro::Promise<void> promise;
+
+    EXPECT_TRUE(requires { promise.return_void(); });
+    EXPECT_TRUE(requires { typename decltype(promise)::value_type; });
+    EXPECT_TRUE((std::same_as<std::suspend_always, decltype(promise.initial_suspend())>));
+    EXPECT_TRUE((std::same_as<tinycoro::FinalAwaiter, decltype(promise.final_suspend())>));
+
+    EXPECT_TRUE((std::same_as<std::coroutine_handle<decltype(promise)>, decltype(promise.get_return_object())>));
+}
+
+TEST(PromiseTest, PromiseTest_int)
+{
+    tinycoro::Promise<int32_t> promise;
+    EXPECT_TRUE(requires { promise.return_value(42); });
+    EXPECT_TRUE(requires { typename decltype(promise)::value_type; });
+
+    promise.return_value(42);
+
+    EXPECT_EQ(promise.ReturnValue(), 42);
+
+    EXPECT_TRUE((std::same_as<std::coroutine_handle<decltype(promise)>, decltype(promise.get_return_object())>));
+
+    EXPECT_TRUE((std::same_as<std::suspend_always, decltype(promise.initial_suspend())>));
+    EXPECT_TRUE((std::same_as<tinycoro::FinalAwaiter, decltype(promise.final_suspend())>));
+}
+
+TEST(PromiseTest, PromiseTest_MoveOnly)
+{
+    struct MoveOnly
+    {
+        MoveOnly(int32_t v)
+        : i{v}
+        {
+        }
+
+        MoveOnly(MoveOnly&&) = default;
+        MoveOnly& operator=(MoveOnly&&) = default;
+
+        int32_t i{};
+    };
+
+    tinycoro::Promise<MoveOnly> promise;
+    EXPECT_TRUE(requires { promise.return_value(MoveOnly{12}); });
+    EXPECT_TRUE(requires { typename decltype(promise)::value_type; });
+
+    promise.return_value(MoveOnly{42});
+
+    auto obj = promise.ReturnValue();
+
+    EXPECT_EQ(obj.i, 42);
+
+    EXPECT_TRUE((std::same_as<std::coroutine_handle<decltype(promise)>, decltype(promise.get_return_object())>));
+
+    EXPECT_TRUE((std::same_as<std::suspend_always, decltype(promise.initial_suspend())>));
+    EXPECT_TRUE((std::same_as<tinycoro::FinalAwaiter, decltype(promise.final_suspend())>));
+}
