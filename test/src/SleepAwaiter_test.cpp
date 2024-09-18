8413f60a5e0acf40ced24f9f5de56406f0699b34
kovacsnador <kovacs.nador@gmail.com>
2024-09-18 21:03:12 +0200

    pipeline fix

--- test/src/SleepAwaiter_test.cpp
+++ test/src/SleepAwaiter_test.cpp
@@ -5,15 +5,18 @@
 
 #include <tinycoro/tinycoro_all.h>
 
-TEST(SimpleSleepAwaiterTest, SimpleSleepAwaiterTest)
+struct SleepAwaiterTest : testing::Test
+{
+    tinycoro::CoroScheduler scheduler{4};
+};
+
+TEST_F(SleepAwaiterTest, SimpleSleepAwaiterTest)
 {
     using namespace std::chrono_literals;
 
     auto timeout = 200ms;
 
     auto sleepTask = [timeout]() -> tinycoro::Task<void> { co_await tinycoro::Sleep(timeout); };
-
-    tinycoro::CoroScheduler scheduler{4};
 
     auto start  = std::chrono::system_clock::now();
     auto future = scheduler.Enqueue(sleepTask());
