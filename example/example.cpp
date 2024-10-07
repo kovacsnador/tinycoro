f1b7fb9b154f9e6c30b871502bd46e83d4498f4f
kovacsnador <kovacs.nador@gmail.com>
2024-10-07 20:25:45 +0200

    add AnyOfAwait

--- example/example.cpp
+++ example/example.cpp
@@ -31,7 +31,7 @@
 #include "VoidTask_example.h"
 
 #include "CustomAwaiter.h"
-#include "tinycoro/AsyncAwaiter.hpp"
+#include "tinycoro/SyncAwait.hpp"
 
 tinycoro::Task<std::string> Example_asyncAwaiter(auto& scheduler)
 {
@@ -41,6 +41,7 @@ tinycoro::Task<std::string> Example_asyncAwaiter(auto& scheduler)
 
     auto tupleResult = co_await tinycoro::SyncAwait(scheduler, task1(), task2(), task3());
 
+    // tuple accumulate
     co_return std::apply(
         []<typename... Ts>(Ts&&... ts) {
             std::string result;
@@ -50,6 +51,67 @@ tinycoro::Task<std::string> Example_asyncAwaiter(auto& scheduler)
         tupleResult);
 }
 
+tinycoro::Task<void> Example_asyncAwaiter2(auto& scheduler)
+{
+    auto task1 = []() -> tinycoro::Task<void> { co_return; };
+    auto task2 = []() -> tinycoro::Task<void> { co_return; };
+    auto task3 = []() -> tinycoro::Task<void> { co_return; };
+
+    co_await tinycoro::SyncAwait(scheduler, task1(), task2(), task3());
+}
+
+tinycoro::Task<void> Example_AnyOfCoAwait(auto& scheduler)
+{
+    SyncOut() << "\n\nExample_AnyOf:\n";
+
+    auto task1 = [](auto duration) -> tinycoro::Task<int32_t> {
+        SyncOut() << "  Coro starting..." << "  Thread id : " << std::this_thread::get_id() << '\n';
+
+        int32_t count{0};
+
+        for (auto start = std::chrono::system_clock::now(); std::chrono::system_clock::now() - start < duration;)
+        {
+            co_await tinycoro::CancellableSuspend{++count};
+        }
+        co_return count;
+    };
+
+    auto results = co_await tinycoro::AnyOfAwait(scheduler, task1(1s), task1(2s), task1(3s));
+
+    auto t1 = std::get<0>(results);
+    auto t2 = std::get<1>(results);
+    auto t3 = std::get<2>(results);
+
+    SyncOut() << "co_return => " << t1 << ", " << t2 << ", " << t3 << '\n';
+}
+
+tinycoro::Task<void> Example_AnyOfCoAwait2(auto& scheduler)
+{
+    SyncOut() << "\n\nExample_AnyOf:\n";
+
+    auto task1 = [](auto duration) -> tinycoro::Task<int32_t> {
+        SyncOut() << "  Coro starting..." << "  Thread id : " << std::this_thread::get_id() << '\n';
+
+        int32_t count{0};
+
+        for (auto start = std::chrono::system_clock::now(); std::chrono::system_clock::now() - start < duration;)
+        {
+            co_await tinycoro::CancellableSuspend{++count};
+        }
+        co_return count;
+    };
+
+    auto stopSource = co_await tinycoro::StopSourceAwaiter{};
+
+    auto results = co_await tinycoro::AnyOfStopSourceAwait(scheduler, stopSource, task1(1s), task1(2s), task1(3s));
+
+    auto t1 = std::get<0>(results);
+    auto t2 = std::get<1>(results);
+    auto t3 = std::get<2>(results);
+
+    SyncOut() << "co_return => " << t1 << ", " << t2 << ", " << t3 << '\n';
+}
+
 int main()
 {
     tinycoro::CoroScheduler scheduler{std::thread::hardware_concurrency()};
@@ -57,6 +119,12 @@ int main()
 
     auto future = scheduler.Enqueue(Example_asyncAwaiter(scheduler));
     std::cout << future.get() << '\n';
+
+    auto future2 = scheduler.Enqueue(Example_asyncAwaiter2(scheduler));
+    future2.get();
+
+    scheduler.Enqueue(Example_AnyOfCoAwait(scheduler)).get();
+    scheduler.Enqueue(Example_AnyOfCoAwait2(scheduler)).get();
 
     /*{
         Example_voidTask(scheduler);
