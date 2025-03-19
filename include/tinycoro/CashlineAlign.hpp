8ee74b48f5e08ffb0583178d4c38c6edf3907644
kovacsnador <kovacs.nador@gmail.com>
2025-03-19 20:44:31 +0100

    add atomic queue to the scheduler

---
+++ include/tinycoro/CashlineAlign.hpp
@@ -0,0 +1,22 @@
+#ifndef __TINY_CORO_CASHLINE_ALIGN_HPP__
+#define __TINY_CORO_CASHLINE_ALIGN_HPP__
+
+#include <new> // std::hardware_destructive_interference_size
+
+namespace tinycoro { namespace detail {
+
+#ifdef __cpp_lib_hardware_interference_size
+    using std::hardware_constructive_interference_size;
+    using std::hardware_destructive_interference_size;
+#else
+    // 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
+    constexpr size_t hardware_constructive_interference_size = 64;
+    constexpr size_t hardware_destructive_interference_size  = 64;
+#endif
+
+    // this is only an estimation....
+    static constexpr size_t CACHELINE_SIZE = hardware_destructive_interference_size;
+
+}} // namespace tinycoro::detail
+
+#endif //!__TINY_CORO_CASHLINE_ALIGN_HPP__
