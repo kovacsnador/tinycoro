df0382129164e02410378f01342bb786d8033e0d
kovacsnador <kovacs.nador@gmail.com>
2025-04-24 08:58:00 +0200

    try to repo use after free in pipeline

--- include/tinycoro/SchedulerWorker.hpp
+++ include/tinycoro/SchedulerWorker.hpp
@@ -170,6 +170,16 @@ namespace tinycoro { namespace detail {
                         continue;
                     }
 
+                    // Can't start a new task if we are in the RESUMING state.
+                    // This can cause a heap-use-after-free in GeneratePauseResume (line 278).
+                    //
+                    // The problem is as follows:
+                    // We want to resume a paused task by pushing it into the _notifiedCachedTasks queue.
+                    // The worker thread may pick it up immediately and finish it very quickly.
+                    // (Most likely, the resumer thread is preempted or yielded by the OS.)
+                    // As a result, the task may complete before the GeneratePauseResume callback has even finished.
+                    //while (_popState.load(std::memory_order_relaxed) == EPopWaitingState::RESUMIMG);
+
                     // get the task from the cache
                     task.reset(_cachedTasks.pop());
                 }
@@ -274,7 +284,9 @@ namespace tinycoro { namespace detail {
                                 // _notifiedCachedTasks queue.
                                 auto failed = !_notifiedCachedTasks.try_push(task.release());
 
-                                // set the state back to IDLE, to allow others to continue
+                                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                                // set the state back to IDLE, to allow others to countinue
                                 _popState.store(EPopWaitingState::IDLE, std::memory_order_release);
 
                                 if (failed)
