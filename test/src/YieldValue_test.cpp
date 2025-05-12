0be6b45edccc6e0fe594998a7067f8fd79c95786
kovacsnador <kovacs.nador@gmail.com>
2025-05-12 08:19:11 +0200

    disable problematic unit test under gcc11

--- test/src/YieldValue_test.cpp
+++ test/src/YieldValue_test.cpp
@@ -2,6 +2,10 @@
 
 #include "tinycoro/tinycoro_all.h"
 
+#if defined(__GNUC__) && !defined(__clang__) && (__GNUC__ == 11)
+#define TINY_CORO_GCC_11
+#endif
+
 struct YieldValueTest : testing::TestWithParam<int32_t>
 {
 };
@@ -22,10 +26,9 @@ TEST_P(YieldValueTest, YieldValueTest_generator)
         co_return max;
     };
 
-    auto consumer = [gen = generator](int32_t max)->tinycoro::Task<void>
-    {   
+    auto consumer = [gen = generator](int32_t max) -> tinycoro::Task<void> {
         auto task = gen(max);
-        for(auto it : std::views::iota(0, max))
+        for (auto it : std::views::iota(0, max))
         {
             EXPECT_EQ(co_await task, it);
         }
@@ -35,6 +38,14 @@ TEST_P(YieldValueTest, YieldValueTest_generator)
 
     tinycoro::GetAll(scheduler, consumer(count));
 }
+
+// Disable code becasue of GCC-11 bug.
+//
+// The compiler error message was to following on the pipeline:
+// tinycoro/test/src/YieldValue_test.cpp:53:20: error: no suspend point info for ''co_await' not supported by dump_decl<declaration error>'
+//
+// See: https://stackoverflow.com/questions/67860049/why-cant-co-await-return-a-string
+#ifndef TINY_CORO_GCC_11
 
 tinycoro::Task<std::variant<int32_t, bool>> YieldCoroutine()
 {
@@ -46,8 +57,7 @@ TEST(YieldValueTest, YieldValueTest_variant_yield)
 {
     tinycoro::Scheduler scheduler;
 
-    auto runner = []()->tinycoro::Task<void>
-    {
+    auto runner = []() -> tinycoro::Task<void> {
         auto task = YieldCoroutine();
 
         auto val = co_await task;
@@ -64,8 +74,7 @@ TEST(YieldValueTest, YieldValueTest_variant_yield)
 
 TEST(YieldValueTest, YieldValueTest_variant_yield_runinline)
 {
-    auto runner = []()->tinycoro::Task<void>
-    {
+    auto runner = []() -> tinycoro::Task<void> {
         auto task = YieldCoroutine();
 
         auto val = co_await task;
@@ -80,12 +89,13 @@ TEST(YieldValueTest, YieldValueTest_variant_yield_runinline)
     tinycoro::RunInline(runner());
 }
 
+#endif /* !TINY_CORO_GCC_11 */
+
 TEST(YieldValueTest, YieldValueTest_direct_invoke_runinline)
 {
-    auto func = []()->tinycoro::Task<std::unique_ptr<int32_t>>
-    {
-        co_yield std::make_unique<int32_t>(41);        
-        co_return std::make_unique<int32_t>(42);        
+    auto func = []() -> tinycoro::Task<std::unique_ptr<int32_t>> {
+        co_yield std::make_unique<int32_t>(41);
+        co_return std::make_unique<int32_t>(42);
     };
 
     // in this case co_yield is just ignored
@@ -97,10 +107,9 @@ TEST(YieldValueTest, YieldValueTest_direct_invoke_scheduler)
 {
     tinycoro::Scheduler scheduler;
 
-    auto func = []()->tinycoro::Task<std::unique_ptr<int32_t>>
-    {
-        co_yield std::make_unique<int32_t>(41);        
-        co_return std::make_unique<int32_t>(42);        
+    auto func = []() -> tinycoro::Task<std::unique_ptr<int32_t>> {
+        co_yield std::make_unique<int32_t>(41);
+        co_return std::make_unique<int32_t>(42);
     };
 
     // in this case co_yield is just ignored
