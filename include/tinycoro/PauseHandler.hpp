a1a440bcc48ddf6fe9c5930574df3b4ecaf85128
kovacsnador <kovacs.nador@gmail.com>
2025-12-22 21:11:09 +0100

    remove pause event from pause handler hpp

--- include/tinycoro/PauseHandler.hpp
+++ include/tinycoro/PauseHandler.hpp
@@ -25,50 +25,6 @@ namespace tinycoro {
 
     } // namespace concepts
 
-    /* namespace detail {
-
-        // This class is intented to handle awaitable
-        // resumption from a paused state.
-        struct ResumeSignalEvent
-        {
-        private:
-            // Need to be up there to make the
-            // "requirement" work for Set().
-            ResumeCallback_t _notifyCallback;
-            mutable std::atomic_flag _flag;
-
-        public:
-            ResumeSignalEvent() = default;
-
-            ResumeSignalEvent(ResumeSignalEvent&& other) noexcept
-            : _notifyCallback{std::move(other._notifyCallback)}
-            {
-            }
-
-            auto Notify(ENotifyPolicy policy = ENotifyPolicy::RESUME) const
-            {
-                assert(_notifyCallback);
-
-                // We notify the coroutine.
-                //
-                // This notify callback is responsible
-                // for the corouitne resumption.
-                return CallOnce(_flag, std::memory_order::acquire, _notifyCallback, policy);
-            }
-
-            // Sets the notify callback
-            template <typename T>
-                requires requires (T&& t) { { _notifyCallback = std::forward<T>(t) }; }
-            void Set(T&& cb)
-            {
-                assert(_flag.test() == false);
-
-                _notifyCallback = std::forward<T>(cb);
-            }
-        };
-
-    } // namespace detail*/
-
     class PauseHandler : public detail::IntrusiveObject<PauseHandler>
     {
         static constexpr uint8_t c_pauseMask{0x01};
